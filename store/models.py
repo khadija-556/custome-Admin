e5eb2ec3fe1ebc803ac3ed05be20bf196a4fb0c3
khadija <khatukhadija556@gmail.com>
2024-05-27 22:56:12 +0600

    third commit

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
+from uuid import uuid4
 
 
 class Promotion(models.Model):
@@ -81,12 +82,15 @@ class OrderItem(models.Model):
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 class Cart(models.Model):
+    id = models.UUIDField(primary_key=True , default=uuid4)
     created_at = models.DateTimeField(auto_now_add=True)
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE , related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
+    class Meta:
+        unique_together = [['cart','product']]
 
 
 class Address(models.Model):
